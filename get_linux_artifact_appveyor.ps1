35496d7eaa4d38fd89bb9452661dfbbaddefe102
D3PSI <d3psigames@gmail.com>
2020-03-20 00:35:19 +0100

    Add appveyor deployment to GitHub releases

---
+++ get_linux_artifact_appveyor.ps1
@@ -0,0 +1,16 @@
+$apiUrl = 'https://ci.appveyor.com/api'
+$token = '9ou7lloef1iiwqiuma58'
+$headers = @{}
+$headers['Authorization'] = "Bearer $token"
+$headers["Content-type"] = "application/json"
+$accountName = 'D3PSI'
+$projectSlug = 'nautilus'
+$downloadLocation = 'C:\projects\nautilus'
+$project = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$accountName/$projectSlug" -Headers $headers
+$jobId = $project.build.jobs[0].jobId
+$artifacts = Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts" -Headers $headers
+$artifactFileName = $artifacts[0].fileName
+$version_appveyor = $env:APPVEYOR_BUILD_VERSION
+$localArtifactPath = "$downloadLocation\nautilus-linux-rolling-$version_appveyor.zip"
+Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts/$artifactFileName" `
+-OutFile $localArtifactPath -Headers @{ "Authorization" = "Bearer $token" }
