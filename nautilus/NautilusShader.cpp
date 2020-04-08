b3daac31f28ea854f60af1e609212e7dba8e4a79
D3PSI <d3psigames@gmail.com>
2020-04-08 14:00:59 +0200

    Add shader component structure

--- nautilus/NautilusShader.cpp
+++ nautilus/NautilusShader.cpp
@@ -3,6 +3,13 @@
 
 #include "NautilusShader.hpp"
 
+NautilusShader::NautilusShader() {
+}
 
+NautilusShader::NautilusShader(NautilusAPI _api) : m_api(_api) {
+}
+
+NautilusShader::~NautilusShader() {
+}
 
 #endif      // NAUTILUS_SHADER_CPP
---
+++ nautilus/NautilusShaderComponent.cpp
@@ -0,0 +1,12 @@
+#ifndef NAUTILUS_SHADER_COMPONENT_CPP
+#define NAUTILUS_SHADER_COMPONENT_CPP
+
+#include "NautilusShaderComponent.hpp"
+
+NautilusShaderComponent::NautilusShaderComponent() {
+}
+
+NautilusShaderComponent::~NautilusShaderComponent() {
+}
+
+#endif      // NAUTILUS_SHADER_COMPONENT_CPP
