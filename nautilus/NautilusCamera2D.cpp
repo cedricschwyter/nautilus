fec29bbbb3f2bfac73ab3d2c03111a611aa21f9b
D3PSI <d3psigames@gmail.com>
2020-07-12 18:56:40 +0200

    Add 2D camera logic

--- nautilus/NautilusCamera2D.cpp
+++ nautilus/NautilusCamera2D.cpp
@@ -15,12 +15,20 @@ void NautilusCamera2D::key(
     int             _scancode, 
     int             _action, 
     int             _mods) {
+        if(_key == GLFW_KEY_W && _action == GLFW_PRESS) this->move(nautilus::NAUTILUS_CAMERA_MOVEMENT_DIRECTION_FORWARD);
+        if(_key == GLFW_KEY_A && _action == GLFW_PRESS) this->move(nautilus::NAUTILUS_CAMERA_MOVEMENT_DIRECTION_LEFT);
+        if(_key == GLFW_KEY_S && _action == GLFW_PRESS) this->move(nautilus::NAUTILUS_CAMERA_MOVEMENT_DIRECTION_BACKWARD);
+        if(_key == GLFW_KEY_D && _action == GLFW_PRESS) this->move(nautilus::NAUTILUS_CAMERA_MOVEMENT_DIRECTION_RIGHT);
 }
 
 void NautilusCamera2D::cursor(GLFWwindow* _window, double _x, double _y) {
 }
 
 void NautilusCamera2D::scroll(GLFWwindow* _window, double _dx, double _dy) {
+    if(this->m_inputEnabled) {
+        this->m_fov -= static_cast< float >(_dy);
+        this->m_fov = std::clamp(this->m_fov, nautilus::defaults::CAMERA_MIN_FOV, nautilus::defaults::CAMERA_MAX_FOV);
+    }
 }
 
 #endif      // NAUTILUS_CAMERA_2D_CPP
