97dd128aa14cb1297419361241231b68ba8c0698
D3PSI <d3psigames@gmail.com>
2020-09-29 21:59:29 +0200

    Add shaders

--- examples/opengl_rubiks_cube_3d/Main.cpp
+++ examples/opengl_rubiks_cube_3d/Main.cpp
@@ -6,6 +6,10 @@
  */ 
 int main() {
     nautilus::NautilusShell* shell = new nautilus::NautilusShellOpenGL();
+    nautilus::NautilusPipeline* pipe = new nautilus::NautilusPipeline("default_3d_pipe");
+    pipe->vertex("res/shaders/opengl/v330_core/standard_3d/standard_3d.glsl.vert");
+    pipe->fragment("res/shaders/opengl/v330_core/standard_3d/standard_3d.glsl.frag");
+    shell->attach(pipe);
     nautilus::NautilusCore::attach(shell);
     return 0;
 }
